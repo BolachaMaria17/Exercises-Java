ada2c7b30a1bae881e41b0dd026b38fcb2184632
Maria Eduarda Dias dos Santos <182100522@senacinformatica.br>
2024-06-14 21:08:06 -0300

    class

---
+++ ex/App.java
@@ -0,0 +1,17 @@
+package ex;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scan = new Scanner(System.in);
+        int number = 0;
+        Modelo modelo = new Modelo();
+
+        System.out.print("Informe um número: ");
+        number = scan.nextInt();
+        modelo.isPrimeNumber(number);
+
+        scan.close();
+    }
+}
